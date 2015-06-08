1aebe774780b5f14b61a8fc32f085990fc318676
Edward Gomez <edward@gomez.me.uk>
2015-06-08 14:40:49 +0100

    Adding licence to files

--- whatsupapp/urls.py
+++ whatsupapp/urls.py
@@ -1,3 +1,19 @@
+'''
+WhatsUP: astronomical object suggestions for Las Cumbres Observatory Global Telescope Network
+Copyright (C) 2014-2015 LCOGT
+
+urls.py 
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+'''
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
